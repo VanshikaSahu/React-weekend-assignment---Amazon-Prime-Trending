9925db7ccee89fa9b0782fc39457d925ff529de0
VanshikaSahu <82844285+VanshikaSahu@users.noreply.github.com>
2021-06-01 22:53:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+const App = (props) => {
+  const[index, setIndex] = useState(0); 
+  
+  const nextSlide = () =>{
+    if(index<props.slides.length-1){
+      setIndex(index+1);
+    }
+  }
+
+  const prevSlide = () =>{
+    index>0 && setIndex(index-1);
+    //can use and operator to render statements.
+
+  }
+
+  const restart = ()=>{
+    setIndex(0);
+  }
+
   return (
-    <></>
+    <>
+      <h1 data-testid="title"> {props.slides[index].title} </h1>
+      <p data-testid="text" > {props.slides[index].text}</p>
+      <button disabled = {index===props.slides.length-1?true:false}onClick = {nextSlide} data-testid="button-next">next</button>
+      <button disabled = {index===0?true:false}  onClick = {prevSlide} data-testid="button-prev">prev</button>
+      <button disabled = {index===0?true:false} onClick = {restart} data-testid="button-restart">restart</button>
+    </>
   )
 }
 
